728d16474115c003fbcf5f9e43ae150976ec69dc
Sady Doucoure <sadydoucoure@gmail.com>
2024-03-31 15:58:04 -0400

    add processFormV2 controller

--- src/main/java/io/monkeybread/controller/HelloWorldController.java
+++ src/main/java/io/monkeybread/controller/HelloWorldController.java
@@ -1,19 +1,35 @@
 package io.monkeybread.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
 
-	@GetMapping(path="/showForm")
+	@GetMapping(path = "/showForm")
 	public String showForm() {
 		return "helloworld-form";
 	}
-	
-	@GetMapping(path="/processForm")
+
+	@GetMapping(path = "/processForm")
 	public String processForm() {
 		return "helloworld";
 	}
-	
+
+	@GetMapping(path = "/processFormV2")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+
+		String theName = request.getParameter("studentName");
+
+		theName = theName.toUpperCase();
+
+		String result = "Yo! " + theName;
+
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+
 }
